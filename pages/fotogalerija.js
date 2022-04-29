003507fabc8681b3c3b329b903b1807bf8c9933f
lorenavucic <102366397+lorenavucic@users.noreply.github.com>
2022-04-29 22:41:18 +0200

    Add files via upload

---
+++ pages/fotogalerija.js
@@ -0,0 +1,9 @@
+function galerija1(){
+    window.location.href="galerija1.html"
+}
+function galerija2(){
+    window.location.href="galerija2.html"
+}
+function galerija3(){
+    window.location.href="galerija3.html"
+}
