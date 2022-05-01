d7d8e77ea3b6ff47610a24c1884d8cca81117425
martaglavan <101876293+martaglavan@users.noreply.github.com>
2022-05-01 17:36:50 +0200

    Add files via upload

---
+++ pages/izlozba.js
@@ -0,0 +1,160 @@
+function playmae() {
+  let mae = new Audio('../audio/mae.mp3');
+  mae.play();
+}
+function playpatricia(){
+    let patricia = new Audio('../audio/patricia.mp3');
+    patricia.play();
+}
+function playcurie(){
+    let curie = new Audio('../audio/curie.mp3');
+    curie.play();
+}
+function playmargarita(){
+    let margarita = new Audio('../audio/margarita.mp3');
+    margarita.play();
+}
+function playrosalind(){
+    let rosalind = new Audio('../audio/rosalind.mp3');
+    rosalind.play();
+}
+function playjane(){
+    let jane = new Audio('../audio/jane.mp3');
+    jane.play();
+}
+function playj(){
+    let jocelyn = new Audio('../audio/j.mp3');
+    jocelyn.play();
+}
+function playchien(){
+    let chien = new Audio('../audio/chien.mp3');
+    chien.play();
+}
+function playgladys(){
+    let gladys = new Audio('../audio/gladys.mp3');
+    gladys.play();
+}
+function playmarie(){
+    let marie = new Audio('../audio/marie.mp3');
+    marie.play();
+}
+function playbath(){
+    let bath=new Audio('../audio/patricia.mp3');
+    bath.play();
+}
+function playsally(){
+    let sally=new Audio('../audio/sally.mp3');
+    sally.play();
+}
+function playgerty(){
+    let gerty=new Audio('../audio/gerty.mp3');
+    gerty.play();
+}
+function playlise(){
+    let lise=new Audio('../audio/lise.mp3');
+    lise.play();
+}
+function palyrachel(){
+    let rachel=new Audio('../audio/rachel.mp3');
+    rachel.play();
+}
+function playsusana(){
+    let susana=new Audio('../audio/susana.mp3');
+    susana.play();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function show1(){
+    document.getElementById("galerija2").style.display="none"
+    document.getElementById("galerija1").style.display="block"
+}
+function show2(){
+    document.getElementById("galerija1").style.display="none"
+    document.getElementById("galerija2").style.display="block"
+    document.getElementById("galerija3").style.display="none"
+}
+function show3(){
+    document.getElementById("galerija2").style.display="none"
+    document.getElementById("galerija3").style.display="block"
+    document.getElementById("galerija4").style.display="none"
+}
+function show4(){
+    document.getElementById("galerija3").style.display="none"
+    document.getElementById("galerija4").style.display="block"
+    document.getElementById("galerija5").style.display="none"
+}
+function show5(){
+    document.getElementById("galerija4").style.display="none"
+    document.getElementById("galerija5").style.display="block"
+    document.getElementById("galerija6").style.display="none"
+}
+function show6(){
+    document.getElementById("galerija5").style.display="none"
+    document.getElementById("galerija6").style.display="block"
+    document.getElementById("galerija7").style.display="none"
+}
+function show7(){
+    document.getElementById("galerija6").style.display="none"
+    document.getElementById("galerija7").style.display="block"
+    document.getElementById("galerija8").style.display="none"
+}
+function show8(){
+    document.getElementById("galerija7").style.display="none"
+    document.getElementById("galerija8").style.display="block"
+    document.getElementById("galerija9").style.display="none"
+}
+function show9(){
+    document.getElementById("galerija8").style.display="none"
+    document.getElementById("galerija9").style.display="block"
+    document.getElementById("galerija10").style.display="none"
+}
+function show10(){
+    document.getElementById("galerija9").style.display="none"
+    document.getElementById("galerija10").style.display="block"
+    document.getElementById("galerija11").style.display="none"
+}
+function show11(){
+    document.getElementById("galerija10").style.display="none"
+    document.getElementById("galerija11").style.display="block"
+    document.getElementById("galerija12").style.display="none"
+}
+function show12(){
+    document.getElementById("galerija11").style.display="none"
+    document.getElementById("galerija12").style.display="block"
+    document.getElementById("galerija13").style.display="none"
+}
+
+function show13(){
+    document.getElementById("galerija12").style.display="none"
+    document.getElementById("galerija13").style.display="block"
+    document.getElementById("galerija14").style.display="none"
+}
+function show14(){
+    document.getElementById("galerija13").style.display="none"
+    document.getElementById("galerija14").style.display="block"
+    document.getElementById("galerija15").style.display="none"
+}
+function show15(){
+    document.getElementById("galerija14").style.display="none"
+    document.getElementById("galerija15").style.display="block"
+    document.getElementById("galerija16").style.display="none"
+}
+function show16(){
+    document.getElementById("galerija15").style.display="none"
+    document.getElementById("galerija16").style.display="block"
+
+}
